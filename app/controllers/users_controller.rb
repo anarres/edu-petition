a9eb43d0089bbe311582c1c37794b66254949b65
Anarres <harrison9340@googlemail.com>
2010-11-21 18:06:42 +0000

    Display 30 entries per page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @users = @users.reverse	# Hacky way to get entries to show in reverse order
-    @users = @users.paginate(:page => params[:page], :per_page => 10)
+    @users = @users.paginate(:page => params[:page], :per_page => 30)
 
     respond_to do |format|
       format.html # index.html.erb
