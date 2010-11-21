24de0e79e6d95ebd144e7b26a2d1634a626c5bd7
Anarres <harrison9340@googlemail.com>
2010-11-21 15:08:13 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EduPetition::Application.config.secret_token = '515fc632a198ad5982a06764c906adf00c974e8e73ced8752e28a6ae32fb795de800c3e3a670b6bac3c6279c36fb8f1fc74e5f45a9f361515e20e722d0a00862'
